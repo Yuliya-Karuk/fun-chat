acec252c7896a4ecb3f9f6e1f2e99bb41dbbae3f
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-04-07 00:01:50 +0400

    chore: configure start setup for project

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[local]',
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/mixins.scss"; @import "./src/styles/placeholders.scss";`,
+      },
+    },
+  },
+  base: ''
+});
