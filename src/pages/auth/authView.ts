b992de122ad79d0ad635a67cd96fad5a668580fa
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-04-09 03:22:27 +0400

    feat: implement basic layout for auth page

--- src/pages/auth/authView.ts
+++ src/pages/auth/authView.ts
@@ -1,16 +1,41 @@
 import { BaseComponent } from '../../components/baseComponent';
+import { LoginForm } from '../../components/loginForm/loginForm';
 import { createElementWithProperties } from '../../utils/utils';
 import styles from './auth.module.scss';
 
 export class AuthView extends BaseComponent {
   public carsBlock: HTMLDivElement;
+  public loginForm: LoginForm;
 
   constructor() {
     super('div', [styles.auth]);
+    this.loginForm = new LoginForm();
   }
 
   public renderContent(): void {
-    const pageName = createElementWithProperties('h2', [styles.heading], undefined, [{ innerText: 'Auth' }]);
-    this.appendChildren([pageName]);
+    this.renderStaticContent();
+    this.loginForm.renderContent();
+    this.appendChildren([this.loginForm.getNode()]);
+  }
+
+  public renderStaticContent(): void {
+    const authImage = createElementWithProperties('div', [styles.authImage]);
+
+    const authHeading = createElementWithProperties('div', [styles.authHeading], undefined, [
+      { innerText: 'Stay connected with your friends and family' },
+    ]);
+    const authText = createElementWithProperties('div', [styles.authText], undefined, [
+      { innerText: 'Secure, private messaging' },
+    ]);
+    const authTextContainer = createElementWithProperties('div', [styles.authTextContainer], undefined, undefined, [
+      authHeading,
+      authText,
+    ]);
+
+    const authContent = createElementWithProperties('div', [styles.authContent], undefined, undefined, [
+      authImage,
+      authTextContainer,
+    ]);
+    this.appendChildren([authContent]);
   }
 }
